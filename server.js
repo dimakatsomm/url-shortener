e727fbd24dbbc8e2c670b697dd01384e107f0c2f
dimakatsomm <dmakgatho2@gmail.com>
2021-06-20 21:26:43 +0200

    Bug fixes

--- server.js
+++ server.js
@@ -44,13 +44,15 @@ const generateRandomString = async (length = getRandomValue(8, 12)) => {
 }
 
 const isValidURL = async (urlString) => {
-  var pattern = new RegExp('^(http{s}:\\/\\/)?'+ // protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|'+ // domain name
-  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  return pattern.test(urlString);
+  let url = null
+
+  try {
+    url = new URL(urlString)
+  } catch (err) {
+    if (err instanceof TypeError) url = 'invalid url'
+  }
+
+  return url === 'invalid url' ? false : true;
 }
 
 app.get('/', (req, res) => {
